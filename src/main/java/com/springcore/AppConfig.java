11f2d6dfb488b49b8a46868593c64b69ba38c866
Junhan0037 <wnsgks0037@naver.com>
2020-09-28 17:25:37 +0900

    UPDATE : Appconfig 리팩토링

--- src/main/java/com/springcore/AppConfig.java
+++ src/main/java/com/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.springcore;
 
+import com.springcore.discount.DiscountPolicy;
 import com.springcore.discount.FixDiscountPolicy;
+import com.springcore.member.MemberRepository;
 import com.springcore.member.MemberService;
 import com.springcore.member.MemberServiceImpl;
 import com.springcore.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
