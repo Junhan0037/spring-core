fbfbe367112ab6ea6c02d0b7ce0e7ee155e7218a
Junhan0037 <wnsgks0037@naver.com>
2020-10-04 17:18:43 +0900

    ADD : @Configuration과 바이트코드 조작의 마법

--- src/test/java/com/springcore/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/springcore/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,14 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
 
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+
 }
