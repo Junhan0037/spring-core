eb89114d7769b6eea636aedb40cfd92602df7b5f
Junhan0037 <wnsgks0037@naver.com>
2020-10-05 15:47:09 +0900

    ADD : @ComponentScan과 의존관계 자동 주입

---
+++ src/test/java/com/springcore/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package com.springcore;
+
+import com.springcore.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
