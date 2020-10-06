fc1db3962ce0e44d8afc082c49b9beb2070a231c
Junhan0037 <wnsgks0037@naver.com>
2020-10-06 16:53:56 +0900

    ADD : 롬복과 최신 트렌드

--- src/main/java/com/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/springcore/order/OrderServiceImpl.java
@@ -3,20 +3,16 @@ package com.springcore.order;
 import com.springcore.discount.DiscountPolicy;
 import com.springcore.member.Member;
 import com.springcore.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
