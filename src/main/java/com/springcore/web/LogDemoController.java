90c046b7b8f58a90bbe12ac7965e93d62261dcb1
Junhan0037 <wnsgks0037@naver.com>
2020-10-09 17:16:47 +0900

    ADD : 스코프와 프록시

--- src/main/java/com/springcore/web/LogDemoController.java
+++ src/main/java/com/springcore/web/LogDemoController.java
@@ -14,12 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
 
         myLogger.setRequestURL(requestURL);
