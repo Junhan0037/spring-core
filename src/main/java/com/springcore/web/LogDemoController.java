a898d43bd0db2bf1a4125f26548b17382c6dfc4b
Junhan0037 <wnsgks0037@naver.com>
2020-10-09 16:56:28 +0900

    ADD : request 스코프 예제 만들기

---
+++ src/main/java/com/springcore/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package com.springcore.web;
+
+import com.springcore.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
