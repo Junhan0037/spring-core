25c763ea5e8ba40b574ef1d567d163183922acd2
Junhan0037 <wnsgks0037@naver.com>
2020-10-09 17:07:09 +0900

    ADD : 스코프와 Provider

--- src/main/java/com/springcore/web/LogDemoController.java
+++ src/main/java/com/springcore/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.springcore.web;
 
 import com.springcore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
+
         return "OK";
     }
 
